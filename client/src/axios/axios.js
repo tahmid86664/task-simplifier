98c70e54aa17144fb414c1564a024df2805d2769
Tahmid Khandokar <tahmidkhandokar82@gmail.com>
2021-04-21 16:34:30 +0600

    modified some settings in package.json for deployment and get ready for deploy

--- client/src/axios/axios.js
+++ client/src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:9000'
+    baseURL: 'https://task-simplifier.herokuapp.com'
 }); // when deploy change the domain with the baseurl
 
 export default instance;
--- server.js
+++ server.js
@@ -4,10 +4,22 @@ import mongoose from 'mongoose';
 import Todos from './todoSchema.js';
 import Users from './userSchema.js';
 import cors from 'cors';
+import path from 'path';
 
 
 // app config
 const app = express();
+
+// serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 const port = process.env.PORT || 9000;
 
 // middleware
