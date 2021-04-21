94bab46f613c79d15904885a29a011387ec7d07b
Tahmid Khandokar <tahmidkhandokar82@gmail.com>
2021-04-22 02:26:13 +0600

    baseURL changed

--- client/src/axios/axios.js
+++ client/src/axios/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    // baseURL: 'https://task-simplifier.herokuapp.com'
-    baseURL: 'http://localhost:9000'
+    baseURL: 'https://task-simplifier.herokuapp.com'
+    // baseURL: 'http://localhost:9000'
 }); // when deploy change the domain with the baseurl
 
 export default instance;
